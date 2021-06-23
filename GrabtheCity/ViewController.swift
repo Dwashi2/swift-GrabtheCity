a028a0555b0633f2d3234cd0cb7a1ca2f0bb92de
Daniel Washington <danielvertigo@hotmail.com>
2021-06-23 17:08:20 -0300

    files

--- GrabtheCity/ViewController.swift
+++ GrabtheCity/ViewController.swift
@@ -5,6 +5,21 @@
 //  Created by Daniel Washington Ignacio on 23/06/21.
 //
 
+/*
+ Write a function to return the city from each of these vacation spots.
+
+ Examples
+
+ grabCity("[Last Day!] Beer Festival [Munich]") ➞ "Munich"
+
+ grabCity("Cheese Factory Tour [Portland]") ➞ "Portland"
+
+ grabCity("[50% Off!][Group Tours Included] 5-Day Trip to Onsen [Kyoto]") ➞ "Kyoto"
+ Notes
+
+ There may be additional brackets, but the city will always be in the last bracket pair.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +27,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.grabCity("[Last Day!] Beer Festival [Munich]"))
+        print(self.grabCity("Cheese Factory Tour [Portland]"))
+        print(self.grabCity("[50% Off!][Group Tours Included] 5-Day Trip to Onsen [Kyoto]"))
     }
-
+    func grabCity(_ str: String) -> String {
+        var arr: [String] = []
+        var newArr: [String] = []
+        arr.append(String(str.split(separator: "[").last ??  ""))
+        for n in arr{
+            for m in n{
+                newArr.append("\(m)")
+            }
+        }
+        newArr.removeLast()
+        return "\(newArr.joined())"
+    }
 
 }
 
